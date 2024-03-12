95962cd4a1111602935a0f0b9df018d7c6aa19b1
nishanthsuraj <nishanthsuraj@gmail.com>
2024-03-12 17:35:20 +0530

    Contains Data Layer Changes : Models, DB Context, Repository, Dtos, AutoMapper Profiles

---
+++ CommandService/Data/Interfaces/ICommandRepository.cs
@@ -0,0 +1,20 @@
+﻿using CommandService.Models;
+
+namespace CommandService.Data.Interfaces
+{
+    public interface ICommandRepository
+    {
+        bool SaveChanges();
+
+        // Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform plat);
+        bool PlatformExits(int platformId);
+        bool ExternalPlatformExists(int externalPlatformId);
+
+        // Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,19 +1,44 @@
 
+using CommandService.Data;
+using CommandService.Data.Extensions;
+using CommandService.Data.Implementations;
+using CommandService.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace CommandService
 {
     public class Program
     {
+        #region Private Constants
+        private const string CommandsConnectionString = "Commands";
+        #endregion
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             #region Developer Added Configurations - 1
-            builder.WebHost.ConfigureKestrel(options =>
+            //// Add services to the container.
+            if (!builder.Environment.IsDevelopment())
             {
-                options.ListenAnyIP(80);
-            });
+                builder.WebHost.ConfigureKestrel(options =>
+                {
+                    options.ListenAnyIP(80);
+                });
 
-            // Add services to the container.
+                Console.WriteLine("--> Using SqlServer Db");
+                builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString(CommandsConnectionString)));
+            }
+            else
+            {
+                Console.WriteLine("--> Using InMemory Db");
+                builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemory"));
+            }
+
+            builder.Services.AddScoped<ICommandRepository, CommandRepository>();
+
+            // AutoMapper
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             #endregion
 
             builder.Services.AddControllers();
@@ -34,6 +59,9 @@ namespace CommandService
 
             app.UseAuthorization();
 
+            #region Developer Added Configurations - 2
+            PrepareDatabase.Seed(app, app.Environment.IsProduction());
+            #endregion
 
             app.MapControllers();
 

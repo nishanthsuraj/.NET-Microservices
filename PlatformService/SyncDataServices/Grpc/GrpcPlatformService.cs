2d8be2169f61d28e2e3b740638c74ca9de9714a8
nishanthsuraj <nishanthsuraj@gmail.com>
2024-03-14 23:49:36 +0530

    Adding gRPC to Platform service

---
+++ PlatformService/SyncDataServices/Grpc/GrpcPlatformService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using PlatformService.Data.Interfaces;
+using PlatformService.Models;
+
+namespace PlatformService.SyncDataServices.Grpc
+{
+    public class GrpcPlatformService : GrpcPlatform.GrpcPlatformBase
+    {
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GrpcPlatformService(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public override Task<PlatformResponse> GetAllPlatforms(GetAllRequest request, ServerCallContext context)
+        {
+            PlatformResponse response = new PlatformResponse();
+            IEnumerable<Platform> platforms = _repository.GetAllPlatforms();
+
+            foreach (Platform plat in platforms)
+            {
+                response.Platform.Add(_mapper.Map<GrpcPlatformModel>(plat));
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
